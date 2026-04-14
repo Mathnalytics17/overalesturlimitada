2f83014dac961480d0c3700e859ab991f70de238
Luis Molina <luisjose0317@gmail.com>
2026-04-14 16:44:15 -0500

    alestur 1.0 cleaned 4

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/php-error.log');
 error_reporting(E_ALL);
 
 require __DIR__ . '/public/index.php';
